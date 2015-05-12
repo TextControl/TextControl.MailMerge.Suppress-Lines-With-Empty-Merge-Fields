32f05a5895202876e60b18fb8b74e824d5c166e1
Gunnar Giffey <gunnar@textcontrol.com>
2015-05-12 15:09:14 +0200

    Added content.

---
+++ Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace tx_emptylines
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textControl1 = new TXTextControl.TextControl();
+            this.buttonBar1 = new TXTextControl.ButtonBar();
+            this.rulerBar2 = new TXTextControl.RulerBar();
+            this.statusBar1 = new TXTextControl.StatusBar();
+            this.rulerBar1 = new TXTextControl.RulerBar();
+            this.mailMerge1 = new TXTextControl.DocumentServer.MailMerge(this.components);
+            this.serverTextControl1 = new TXTextControl.ServerTextControl();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.mergeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mergeNowToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textControl1
+            // 
+            this.textControl1.ButtonBar = this.buttonBar1;
+            this.textControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textControl1.Font = new System.Drawing.Font("Arial", 10F);
+            this.textControl1.Location = new System.Drawing.Point(25, 77);
+            this.textControl1.Name = "textControl1";
+            this.textControl1.RulerBar = this.rulerBar2;
+            this.textControl1.Size = new System.Drawing.Size(844, 423);
+            this.textControl1.StatusBar = this.statusBar1;
+            this.textControl1.TabIndex = 0;
+            this.textControl1.VerticalRulerBar = this.rulerBar1;
+            // 
+            // buttonBar1
+            // 
+            this.buttonBar1.BackColor = System.Drawing.SystemColors.Control;
+            this.buttonBar1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonBar1.Location = new System.Drawing.Point(0, 24);
+            this.buttonBar1.Name = "buttonBar1";
+            this.buttonBar1.Size = new System.Drawing.Size(869, 28);
+            this.buttonBar1.TabIndex = 1;
+            this.buttonBar1.Text = "buttonBar1";
+            // 
+            // rulerBar2
+            // 
+            this.rulerBar2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.rulerBar2.Location = new System.Drawing.Point(0, 52);
+            this.rulerBar2.Name = "rulerBar2";
+            this.rulerBar2.Size = new System.Drawing.Size(869, 25);
+            this.rulerBar2.TabIndex = 4;
+            this.rulerBar2.Text = "rulerBar2";
+            // 
+            // statusBar1
+            // 
+            this.statusBar1.BackColor = System.Drawing.SystemColors.Control;
+            this.statusBar1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.statusBar1.Location = new System.Drawing.Point(0, 500);
+            this.statusBar1.Name = "statusBar1";
+            this.statusBar1.Size = new System.Drawing.Size(869, 22);
+            this.statusBar1.TabIndex = 2;
+            // 
+            // rulerBar1
+            // 
+            this.rulerBar1.Alignment = TXTextControl.RulerBarAlignment.Left;
+            this.rulerBar1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.rulerBar1.Location = new System.Drawing.Point(0, 77);
+            this.rulerBar1.Name = "rulerBar1";
+            this.rulerBar1.Size = new System.Drawing.Size(25, 423);
+            this.rulerBar1.TabIndex = 3;
+            this.rulerBar1.Text = "rulerBar1";
+            // 
+            // mailMerge1
+            // 
+            this.mailMerge1.RemoveEmptyFields = false;
+            this.mailMerge1.RemoveTrailingWhitespace = false;
+            this.mailMerge1.ReportDataSourceConfig = "";
+            this.mailMerge1.TemplateFile = "template.docx";
+            this.mailMerge1.TextComponent = this.serverTextControl1;
+            // 
+            // serverTextControl1
+            // 
+            this.serverTextControl1.SpellChecker = null;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mergeToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(869, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // mergeToolStripMenuItem
+            // 
+            this.mergeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mergeNowToolStripMenuItem});
+            this.mergeToolStripMenuItem.Name = "mergeToolStripMenuItem";
+            this.mergeToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.mergeToolStripMenuItem.Text = "Merge";
+            // 
+            // mergeNowToolStripMenuItem
+            // 
+            this.mergeNowToolStripMenuItem.Name = "mergeNowToolStripMenuItem";
+            this.mergeNowToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.mergeNowToolStripMenuItem.Text = "Merge now";
+            this.mergeNowToolStripMenuItem.Click += new System.EventHandler(this.mergeNowToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(869, 522);
+            this.Controls.Add(this.textControl1);
+            this.Controls.Add(this.rulerBar1);
+            this.Controls.Add(this.rulerBar2);
+            this.Controls.Add(this.buttonBar1);
+            this.Controls.Add(this.statusBar1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Remove empty field lines";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TXTextControl.TextControl textControl1;
+        private TXTextControl.ButtonBar buttonBar1;
+        private TXTextControl.RulerBar rulerBar2;
+        private TXTextControl.StatusBar statusBar1;
+        private TXTextControl.RulerBar rulerBar1;
+        private TXTextControl.DocumentServer.MailMerge mailMerge1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem mergeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mergeNowToolStripMenuItem;
+        private TXTextControl.ServerTextControl serverTextControl1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TXTextControl;
+
+namespace tx_emptylines
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // dummy data source class 
+        public List<Report> reports = new List<Report>();
+
+        public class Report
+        {
+            public string company { get; set; }
+            public string firstname { get; set; }
+            public string name { get; set; }
+            public string street { get; set; }
+            public string country { get; set; }
+        }
+
+        private void mergeNowToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // create a new dummy data object
+            Report report = new Report();
+            report.company = "Text Control LLC";
+            report.street = "123 Shannon-Willow Rd";
+            report.country = "United States";
+
+            reports.Add(report);
+            
+            // merge the template with data
+            serverTextControl1.Create();
+            mailMerge1.MergeObjects(reports);
+
+            // remove empty lines
+            RemoveEmptyFieldLines();
+
+            // visualize the resulting document
+            byte[] data;
+            serverTextControl1.Save(out data, BinaryStreamType.InternalUnicodeFormat);
+            textControl1.Load(data, BinaryStreamType.InternalUnicodeFormat);
+        }
+
+        private void RemoveEmptyFieldLines()
+        {
+            foreach (IFormattedText obj in serverTextControl1.TextParts)
+            {
+                foreach (ApplicationField field in obj.ApplicationFields)
+                {
+                    // check, if character next to empty field is a carriage return
+                    if (obj.TextChars[field.Start + field.Length].Char == '\r')
+                    {
+                        bool bCompleteLine =
+                            (field.Start == obj.Lines.GetItem(field.Start).Start)
+                            ? true : false;
+
+                        // if yes, remove the field
+                        obj.Selection.Start = field.Start;
+                        obj.ApplicationFields.Remove(field, false);
+
+                        // if the field is the only text in the line
+                        // remove the CR
+                        if (bCompleteLine)
+                        {
+                            obj.Selection.Length = 1;
+                            obj.Selection.Text = "";
+                        }
+
+                        // call RemoveEmptyFieldLines recursively
+                        RemoveEmptyFieldLines();
+                    }
+                    else
+                        field.Text = "";
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            TXTextControl.LoadSettings ls = new LoadSettings();
+            ls.ApplicationFieldFormat = ApplicationFieldFormat.MSWord;
+            textControl1.Load(mailMerge1.TemplateFile, StreamType.WordprocessingML, ls);
+        }
+    }
+}
